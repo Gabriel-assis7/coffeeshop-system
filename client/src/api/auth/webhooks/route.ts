4433460a08632526c73bc862f969b9e44c55d799
Gabriel Assis <cardimg06@gmail.com>
2025-02-20 14:20:54 -0300

    feat(client): add a navbar component

--- client/src/api/auth/webhooks/route.ts
+++ client/src/api/auth/webhooks/route.ts
@@ -1,6 +1,7 @@
 import { env } from "@/data/env/server"
 import { syncClerkMetadata } from "@/services/clerk"
 import { WebhookEvent } from "@clerk/nextjs/server"
+import axios from "axios"
 import { headers } from "next/headers"
 import { Webhook } from "svix"
 
@@ -35,71 +36,50 @@ export async function POST(req: Request) {
     })
   }
 
-  switch (event.type) {
-    case "user.created":
-    case "user.updated": {
-      const email = event.data.email_addresses.find(
-        email => email.id === event.data.primary_email_address_id
-      )?.email_address
-      const firstName = event.data.first_name ?? "";
-      const lastName = event.data.last_name ?? "";
-      const name = `${firstName} ${lastName}`.trim();
-
-      if (email == null) return new Response("No email", { status: 400 })
-      if (name === "") return new Response("No name", { status: 400 })
-
-      if (event.type === 'user.created') {
-        const response = await fetch('http://localhost:4000/api/v1/users', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
+  try {
+    switch (event.type) {
+      case "user.created":
+      case "user.updated": {
+        const email = event.data.email_addresses.find(
+          email => email.id === event.data.primary_email_address_id
+        )?.email_address
+        const firstName = event.data.first_name ?? "";
+        const lastName = event.data.last_name ?? "";
+        const name = `${firstName} ${lastName}`.trim();
+  
+        if (email == null) return new Response("No email", { status: 400 })
+        if (name === "") return new Response("No name", { status: 400 })
+  
+        if (event.type === 'user.created') {
+          const { data: user } = await axios.post('http://localhost:4000/api/v1/users', {
             clerkUserId: event.data.id,
-            name,
-            email,
-            role: 'consumer',
-            image_url: event.data.image_url,
-          }),
-        })
-
-        if (!response.ok) {
-          console.error(`Error creating user: ${response.statusText}`);
-          return new Response('Error creating user', { status: 500 })
-        }
-
-        const user = await response.json();
-
-        await syncClerkMetadata(user);
-      } else {
-        await fetch(`http://localhost:4000/api/v1/users/${event.data.id}`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
+              name,
+              email,
+              role: 'consumer',
+              image_url: event.data.image_url,
+          })
+          await syncClerkMetadata(user)
+        } else {
+          await axios.put(`http://localhost:4000/api/v1/users${event.data.id}`, {
             email,
             name,
             image_url: event.data.image_url,
             role: event.data.public_metadata.role
-          }),
-        })
+          })
+        }
+        break
       }
-      break
-    }
-    case 'user.deleted': {
-      if (event.data.id != null) {
-        await fetch(`http://localhost:4000/api/v1/users/${event.data.id}`, {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({}),
-        })
+      case 'user.deleted': {
+        if (event.data.id != null) {
+          await axios.delete(`http://localhost:4000/api/v1/users${event.data.id}`)
+        }
+        break
       }
-      break
     }
+  } catch (error) {
+    console.error("Api processing failed", error)
+    return new Response("Error processing webhook", { status: 500 })
   }
-
+  
   return new Response('', { status: 200 })
 }
--- client/src/app/(consumer)/layout.tsx
+++ client/src/app/(consumer)/layout.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { Suspense } from "react";
+
 export default async function ConsumerLayout({
   children,
 }: {
@@ -12,5 +18,61 @@ export default async function ConsumerLayout({
 }
 
 export function NavBar() {
-  return <h1>hi</h1>;
+  return (
+    <header className="flex justify-between items-center text-white bg-black h-14 z-10">
+      <div className="flex container justify-between mx-auto items-center">
+        <Link href={"/"} className="text-white text-xl font-medium">
+          Coffee Shop
+        </Link>
+        <nav className="hidden md:flex">
+          <ul className="flex space-x-12">
+            <Link className="hover:text-blue-100" href={"/"}>
+              Home
+            </Link>
+            <Link className="hover:text-blue-100" href={"/"}>
+              Menu
+            </Link>
+            <Link className="hover:text-blue-100" href={"/"}>
+              About Us
+            </Link>
+            <Link className="hover:text-blue-100" href={"/"}>
+              Order
+            </Link>
+            <Link className="hover:text-blue-100" href={"/"}>
+              Contact
+            </Link>
+          </ul>
+        </nav>
+        <div className="flex items-center space-x-4">
+          <Suspense>
+            <SignedOut>
+              <Button asChild>
+                <SignInButton>Sign In</SignInButton>
+              </Button>
+            </SignedOut>
+          </Suspense>
+          <Suspense>
+            <SignedIn>
+              <Link href={"/purchases"}>My orders</Link>
+            </SignedIn>
+          </Suspense>
+          <Suspense
+            fallback={<Skeleton className="w-6 h-6 rounded-md bg-gray-300" />}
+          >
+            <SignedIn>
+              <div className="size-8">
+                <UserButton
+                  appearance={{
+                    elements: {
+                      userButtonAvatarBox: { width: "100%", height: "100%" },
+                    },
+                  }}
+                />
+              </div>
+            </SignedIn>
+          </Suspense>
+        </div>
+      </div>
+    </header>
+  );
 }
